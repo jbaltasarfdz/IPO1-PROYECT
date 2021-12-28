ef3aa6f2e9677b93fabc9260afe163e7ae5c3bf3
CarlosGomezFernandez <carlos.gomez33@alu.uclm.es>
2021-12-28 18:21:05 +0100

    Cambio del foco entre los textbox del login e intento de feedback de error al logearse

--- Proyecto/MainWindow.xaml.cs
+++ Proyecto/MainWindow.xaml.cs
@@ -20,9 +20,13 @@ namespace Proyecto
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string usuario = "admin";
+        private string password = "ipo1";
+
         public MainWindow()
         {
             InitializeComponent();
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -30,12 +34,69 @@ namespace Proyecto
             animalWindow animalwindow = new animalWindow();
             animalwindow.Show();
             
-            
+        }
+
+        /// Mover foco al pulsar enter
+        private void txtUsuario_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                //lblEstado.Content = "Se pulsó el enter ";
+                if (!String.IsNullOrEmpty(txtUsuario.Text)
+                && ComprobarEntrada(txtUsuario.Text, usuario,
+                txtUsuario))
+                {
+                    // habilitar entrada de contraseña y pasarle el foco
+                    txtContrasena.IsEnabled = true;
+                    txtContrasena.Focus();
+                    // deshabilitar entrada de login
+                    txtUsuario.IsEnabled = false;
+                }
+            }
+        }
+
+        /// Mejora en la usabilidad del login
+        private Boolean ComprobarEntrada(string valorIntroducido, string valorValido, Control componenteEntrada)
+        {
+            Boolean valido = false;
+            componenteEntrada.BorderThickness = new Thickness(2);
+            if (valorIntroducido.Equals(valorValido))
+            {
+                // borde y background en verde
+                componenteEntrada.BorderBrush = Brushes.Green;
+                componenteEntrada.Background = Brushes.LightGreen;
+                // imagen al lado de la entrada de usuario --> check
+                //imagenFeedBack.Source = imagCheck;
+                valido = true;
+            }
+            else
+            {
+                // marcamos borde en rojo
+                componenteEntrada.BorderBrush = Brushes.Red;
+                // imagen al lado de la entrada de usuario --> cross
+                //imagenFeedBack.Source = imagCross;
+                valido = false;
+            }
+            return valido;
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+    }
+
+    class ReglaRangoLongitudCaracteres : ValidationRule
+    {
+        public int MinimoCaracteres { set; get; }
+        public int MaximoCaracteres { set; get; }
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            int longitud = ((string)value).Length;
+            if ((longitud < MinimoCaracteres) || (longitud > MaximoCaracteres))
+                return new ValidationResult(false, "Longitud: " +
+                MinimoCaracteres.ToString() + "-" + MaximoCaracteres.ToString() + " dígitos");
+            return new ValidationResult(true, null);
         }
     }
 }
