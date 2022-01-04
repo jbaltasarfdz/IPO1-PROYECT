03cd36c47422eececd179c4c49fa0c4940328a02
CarlosGomezFernandez <carlos.gomez33@alu.uclm.es>
2022-01-04 19:57:18 +0100

    Cambios

--- Proyecto/Socio.cs
+++ Proyecto/Socio.cs
@@ -13,18 +13,18 @@ namespace Proyecto
         public string apellidos { set; get; }
         public string sexo { set; get; }
         public string ID { set; get; }
-        public int edad { set; get; }
+        public string edad { set; get; }
         public string telefono { set; get; }
         public string correo { set; get; }
         public string cuenta { set; get; }
-        public int ayuda { set; get; }
+        public string ayuda { set; get; }
         public string pago { set; get; }
         public string tipo { set; get; }
         public Uri Imagen { set; get; }
         public BitmapImage Foto {set; get; }
     
 
-        public Socio(string nombre, string apellidos, string sexo, string ID, int edad, string telefono, string correo, string cuenta, int  ayuda, string pago, string tipo, Uri Imagen)
+        public Socio(string nombre, string apellidos, string sexo, string ID, string edad, string telefono, string correo, string cuenta, string ayuda, string pago, string tipo, Uri Imagen)
         {
             this.nombre = nombre;
             this.apellidos = apellidos;
